db5f2fe35224dad24d1519e27c41f2a9168490c9
cpthorsebagel <grimmm94@gmail.com>
2022-03-30 18:59:48 -0700

    Minor updates to home page after MS Teams session  - Render username to home page  - Added styling to home page

--- ReactWarehouseManager/ReactWarehouseManager/src/components/Home.jsx
+++ ReactWarehouseManager/ReactWarehouseManager/src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
+import '../styles/Home.scss';
 
 export default function Inventory(props) {
+  const { user, navigate, setTitle } = props;
+
   useEffect(() => {
-    const { user, navigate, setTitle } = props;
-    console.log(user);
     setTitle('Home');
     if (!user || user.type !== 'admin') {
       navigate('/404');
@@ -11,8 +12,15 @@ export default function Inventory(props) {
   }, [props]);
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="content">
+        <h1>Welcome to Warehouse Manager</h1>
+        <h2>
+          You are logged in as
+          {' '}
+          {user.username}
+        </h2>
+      </div>
     </div>
   );
 }
