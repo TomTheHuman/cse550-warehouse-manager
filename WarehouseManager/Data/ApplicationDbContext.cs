3655ce8e82561a9e4d87b3522c087641be544e7c
Thomas Shaw <thomas@tomthehuman.com>
2022-04-05 20:50:35 -0700

    Recreated Projected Structure
    The previous project had a few bugs such as inconsistent folder names to be fixed.
    There were also configurations required for user authentication that
    needed to be set from the beginning.
    Lastly we had team members using Mac that couldn't use .NET 6.0 with React.
    
    The project now uses a simpler structure (ASP.NET with React template),
    .NET Core 5.0, and includes api-authentication tools for the front end.
    
    - Moved over old React front end files
    - Made adjustments to package.json and scripts
    - Fixed initial conflicts due to merging two different projects
    - Got development environment to run successfully

---
+++ WarehouseManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseManager.Models;
+
+namespace WarehouseManager.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
