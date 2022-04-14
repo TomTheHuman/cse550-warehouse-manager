7206222816e74d6a9f793f7eba7e473b55705bc3
NickGittings <38570076+NickGittings@users.noreply.github.com>
2022-04-13 21:10:28 -0400

    Added InventoryItem Repo and Controller
    Not sure if we want to have all the different objects in one controller or not.

---
+++ WarehouseManager/Controllers/DataController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseManager.Data;
+using WarehouseManager.Models;
+
+namespace WarehouseManager.Controllers
+{
+    [Route("api/")]
+    public class DataController : Controller
+    {
+
+        private readonly SQLInventoryItemRepository _InventoryItemRepository;
+
+        public DataController(SQLInventoryItemRepository inventoryItemRepository)
+        {
+            this._InventoryItemRepository = inventoryItemRepository;
+        }
+
+        [HttpGet("GetInventoryItems")]
+        public IEnumerable<InventoryItem> GetInventoryItems()
+        {
+            return _InventoryItemRepository.GetInventoryItems().ToList();
+        }
+    }
+}
